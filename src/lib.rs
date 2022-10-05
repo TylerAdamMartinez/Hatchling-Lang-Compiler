a043db8fc3038d6c9b4c4612011378f45cd86872
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2022-10-05 09:54:26 -0500

    [lib.rs] Add character, message, and location objects

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,41 @@
+use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
+
+type SerdeUuid = [u8; 16];
 
 #[wasm_bindgen]
 pub fn add(left: usize, right: usize) -> usize {
     left + right
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Location {
+    x: u32,
+    y: u32,
+    character_id: SerdeUuid,
+    order: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Message {
+    message: String,
+    character_id: SerdeUuid,
+    order: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Character {
+    hair_color: String,
+    eye_color: String,
+    skin_color: String,
+    outfit: String,
+    pub id: SerdeUuid,
+    pub position: Location,
+}
+
+#[wasm_bindgen]
+pub fn moch() -> Result<String, String> {
+    todo!()
 }
 
 #[cfg(test)]
